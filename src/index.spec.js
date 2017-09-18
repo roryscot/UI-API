bb14402d732b5d33365d56ca1096b93451c8ee75
Rory Dahl <rsdah13@gmail.com>
2017-09-18 01:19:53 -0700

    test set-up

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,7 @@
 var {expect} = require('chai');
 var jsdom = require("node-jsdom");
 var fs = require('fs');
+var isomorphic = require('isomorphic-fetch');
 
 describe('sanity check', function() {
   it('should pass', function() {
@@ -22,7 +23,7 @@ describe('interface.html', function() {
 
 describe('Built index.html', function() {
   var interfaceHTML = fs.readFileSync('src/builds/index.html', 'utf-8');
-  it('basic test', function(done) {
+  it('Should have main and vendor scripts', function(done) {
     jsdom.env(interfaceHTML, function(err, window) {
       var scripts = window.document.getElementsByTagName('script');
       expect(scripts.length).to.equal(2);
